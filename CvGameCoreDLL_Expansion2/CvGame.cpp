adcda0a8d0364edb7c9e5450f147e65b1118b944
davenye <davenye@users.noreply.github.com>
2018-03-26 01:00:55 +1100

    reset static processPlayerAutoMoves lazy fix

--- CvGameCoreDLL_Expansion2/CvGame.cpp
+++ CvGameCoreDLL_Expansion2/CvGame.cpp
@@ -9211,6 +9211,8 @@ void CvGame::updateMoves()
 	int iI;
 
 	static bool processPlayerAutoMoves = false;	
+	if (m_lastTurnAICivsProcessed == -1) // i.e. just loaded
+		processPlayerAutoMoves = false; // this value could already be true if game reloaded so we gotta reset it. Probably should just be a member but why wasn't it already?
 
 	// Process all AI first, then process players.
 	// Processing of the AI 'first' only occurs when the AI are activated first
