b9591ba26c7ce2352b3f63591ac555c96cb4326b
davenye <davenye@users.noreply.github.com>
2018-04-06 21:47:14 +1000

    cleanup

--- CvGameCoreDLL_Expansion2/CvPlot.cpp
+++ CvGameCoreDLL_Expansion2/CvPlot.cpp
@@ -11004,17 +11004,17 @@ PlotVisibilityChangeResult CvPlot::changeVisibilityCount(TeamTypes eTeam, int iC
 	//remember the maximum
 	m_aiVisibilityCountThisTurnMax[eTeam] = max(m_aiVisibilityCountThisTurnMax[eTeam], m_aiVisibilityCount[eTeam]);
 
-	if (bAlwaysSeeInvisible && eSeeInvisible != NO_INVISIBLE)
+	if(bAlwaysSeeInvisible && eSeeInvisible != NO_INVISIBLE)
 	{
 		NET_MESSAGE_DEBUG_OSTR_ALWAYS("!!!!!!!!!!!!!! BUG EFFECT (bAlwaysSeeInvisible && eSeeInvisible != NO_INVISIBLE) !!!!!!!!!!!!!!!!!!!!!" << eTeam << " " << iChange << " " << eSeeInvisible);
 		if(pUnit)
 			NET_MESSAGE_DEBUG_OSTR_ALWAYS("!!!!!!!!!!!!!! BUG EFFECT (bAlwaysSeeInvisible && eSeeInvisible != NO_INVISIBLE) !!!!!!!!!!!!!!!!!!!!! unit was: " << pUnit->GetID());
 	}
-	if (bAlwaysSeeInvisible)
+	if(bAlwaysSeeInvisible)
 	{
-		for (int iI = 0; iI < NUM_INVISIBLE_TYPES; iI++)
+		for(int iI = 0; iI < NUM_INVISIBLE_TYPES; iI++)
 		{
-			changeInvisibleVisibilityCount(eTeam, (InvisibleTypes)iI, iChange);
+			changeInvisibleVisibilityCount(eTeam, (InvisibleTypes) iI, iChange);
 		}
 	}
 	else if (eSeeInvisible != NO_INVISIBLE)
